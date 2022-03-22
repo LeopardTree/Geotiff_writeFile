b54dde53150a63b556e82d65dbc2aa0bec8f725e
Ludvig Viveland <ludvigvive@gmail.com>
2022-03-22 17:35:57 +0100

    add more metadata properties. but still not a perfect picture.

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ const main = async () => {
   const layer = await image.readRasters();
   const raster = layer[0];
 
+
   //console.log(image);
   const values = raster;
   const imageHeight = await layer.height;
   const imageWidth = await layer.width;
-  console.log(imageWidth, imageHeight);
-  const geoKeys = await image.getGeoKeys();
-  const fileDir = await image.getFileDirectory();
-  const stripOffsets = await fileDir.stripOffsets;
-  const stripByteCounts = await fileDir.stripByteCounts;
+  const geoKeys = await tif.geoKeys;
+  let fileDir = await image.getFileDirectory();
+  let stripOffsets = await image.stripOffsets;
+  let stripByteCounts = await fileDir.stripByteCounts;
   const XResolution = await fileDir.XResolution;
   const YResolution = await fileDir.YResolution;
   const ModelPixelScale = await fileDir.ModelPixelScale;
@@ -48,45 +48,90 @@ const main = async () => {
   const Compression = await fileDir.Compression;
   const SamplesPerPixel = await fileDir.SamplesPerPixel;
   const Orientation = 1;
-  GeoKeyDirectory.forEach(element => {
-    console.log(element);
-  });
+  const ModelTransformation = await fileDir.ModelTransformation;
+  const GDAL_METADATA = await fileDir.GDAL_METADATA;
+  const Software = await fileDir.software;
+  const ProjectedCSTypeGeoKey = await image.geoKeys.ProjectedCSTypeGeoKey;
+
+  // console.log(ModelTransformation);
+
+  // console.log(tif);
+  // console.log(image);
+  // console.dir('geoKeyDirectory: ' + GeoKeyDirectory);
+  // console.log(GeoAsciiParams);
+
   const sweref_tm = 3006;
 
-  const metadata = {
-    height: imageHeight,
-    width: imageWidth,
-    StripOffsets: stripOffsets,
-    StripByteCounts: stripByteCounts,
-    XResolution: XResolution,
-    YResolution: YResolution,
-    ModelPixelScale: ModelPixelScale,
-    ModelTiepoint: ModelTiepoint,
-    GeoKeyDirectory: GeoKeyDirectory,
+  // const metadata = {
+  //   height: imageHeight,
+  //   width: imageWidth,
+  //   StripOffsets: stripOffsets,
+  //   StripByteCounts: stripByteCounts,
+  //   XResolution: XResolution,
+  //   YResolution: YResolution,
+  //   ModelPixelScale: ModelPixelScale,
+  //   ModelTiepoint: ModelTiepoint,
+  //   GeoKeyDirectory: GeoKeyDirectory,
+  //   BitsPerSample: BitsPerSample,
+  //   GeoAsciiParams: GeoAsciiParams,
+  //   PlanarConfiguration: PlanarConfiguration,
+  //   PhotometricInterpretation: 1,
+  //   Compression: Compression,
+  //   SamplesPerPixel: SamplesPerPixel,
+  //   Orientation: Orientation,
+  //   ProjectedCSTypeGeoKey: ProjectedCSTypeGeoKey,
+  //   DateTime: '',
+  //   //ProjectedCSTypeGeoKey: 3006,
+  //   //ProjLinearUnitsGeoKey: 9001
+
+  // }
+
+  // const metadata = {};
+  // await Object.assign(metadata, image.fileDirectory);
+
+  // if(metadata.ColorMap){
+  //   delete metadata.ColorMap;
+  // }
+  // console.log(metadata);
+
+
+  const metadata ={
+    ImageWidth: 500,
+    ImageLength: 500,
     BitsPerSample: BitsPerSample,
+    Compression: 1,
+    PhotometricInterpretation: 3,
+    StripOffsets: await image.fileDirectory.StripOffsets,
+    SamplesPerPixel: 1,
+    RowsPerStrip: 16,
+    StripByteCounts: await image.fileDirectory.StripByteCounts,
+    PlanarConfiguration: 1,
+    ResolutionUnit: 2,
+    Software: await image.fileDirectory.Software,
+    SampleFormat: [ 1 ],
+    ModelPixelScale: [ 10, 10, 0 ],
+    ModelTiepoint: [ 0, 0, 0, 550000, 6455000, 0 ],
+    GeoKeyDirectory: await image.fileDirectory.GeoKeyDirectory,
     GeoAsciiParams: GeoAsciiParams,
-    PlanarConfiguration: PlanarConfiguration,
-    PhotometricInterpretation: PhotometricInterpretation,
-    Compression: Compression,
-    SamplesPerPixel: SamplesPerPixel,
-    Orientation: Orientation,
-    ProjectedCSTypeGeoKey: sweref_tm
+    ProjectedCSTypeGeoKey: 3006
 
   }
-  
+
+  console.log(fileDir);
   //const buffer = new Uint8Array(arrayBuffer);
 
-  //fs.createWriteStream('test3.tif').write(buffer);
+  //
 
   const arrayBuffer = await writeArrayBuffer(raster, metadata);
   console.log(arrayBuffer);
   const buf = await Buffer.from(arrayBuffer);
-  await fs.writeFile('test5.tif', buf, (err) => {
-    if(err){
-      console.log(err);
-    }
+  // await fs.writeFile('nmd_copy4.tif', buf, (err) => {
+  //   if(err){
+  //     console.log(err);
+  //   }
     
-  })
+  // })
+  fs.createWriteStream('test4.tif').write(buf);
 }
 const resulthandler = err => {
   //console.timeEnd('Duration to execute everything');
